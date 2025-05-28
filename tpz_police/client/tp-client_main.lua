f0573ad3fd11e3e73521c6e73b4d3d435366a4ea
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-28 13:16:38 +0300

    Update tp-client_main.lua

--- tpz_police/client/tp-client_main.lua
+++ tpz_police/client/tp-client_main.lua
@@ -3,27 +3,29 @@ local TPZ = exports.tpz_core:getCoreAPI()
 local PlayerData = { 
     IsBusy    = false,
 
-    IsLocationBusy       = false,
-    LocationIndex        = 0, -- Config.Locations
+    IsLocationBusy           = false,
+    LocationIndex            = 0, -- Config.Locations
 
-    SelectedVehicleIndex = 1,
+    SelectedVehicleIndex     = 1,
 	SelectedVehicleNameIndex = nil,
 
-    VehicleEntity        = nil,
-	VehicleEntityModel   = nil,
-    CameraHandler        = nil,
+    VehicleEntity            = nil,
+	VehicleEntityModel       = nil,
+    CameraHandler            = nil,
 
-    Job       = nil,
-    JobGrade  = 0,
+    Job                      = nil,
+    JobGrade                 = 0,
 
-    IsHandcuffed = false,
-    IsShowingBadge = false,
+    IsHandcuffed             = false,
+    IsShowingBadge           = false,
 
-    ThreadsRunning = false,
+    ThreadsRunning           = false,
 
-    LocationIndex = nil,
+    LocationIndex            = nil,
 
-    Loaded    = true -- FIX!
+    IsJailed                 = false,
+
+    Loaded                   = false,
 }
 
 -----------------------------------------------------------
@@ -42,6 +44,34 @@ local DoesHavePermittedJob = function()
 	end
 
 	return false
+
+end
+
+local GetNearbyJail = function()
+    local player          = PlayerPedId()
+    local coords          = GetEntityCoords(player)
+    local closestDistance = math.huge
+    local closestLocation = nil
+
+    for _, location in pairs(Config.Jails) do
+
+        local locationCoords = vector3(location.JailInCoords.x, location.JailInCoords.y, location.JailInCoords.z)
+        local currentDistance = #(coords - locationCoords)
+    
+        if currentDistance <= closestDistance then
+
+            closestDistance = currentDistance
+            closestLocation = location
+
+        end
+
+    end
+
+    if closestLocation then
+        return closestLocation
+    else
+        return false
+    end
 
 end
 
@@ -83,6 +113,8 @@ AddEventHandler("tpz_core:isPlayerReady", function()
 
     end
 
+    PlayerData.Loaded = true
+
 end)
 
 if Config.DevMode then
@@ -111,6 +143,8 @@ if Config.DevMode then
             end
 
         end
+
+        PlayerData.Loaded = true
 
     end)
 
@@ -141,3 +175,68 @@ AddEventHandler("tpz_core:getPlayerJob", function(data)
     end
     
 end)
+
+-----------------------------------------------------------
+--[[ General Events ]]--
+-----------------------------------------------------------
+
+RegisterNetEvent("tpz_police:client:setCharacterAsJailed")
+AddEventHandler("tpz_police:client:setCharacterAsJailed", function(isJailed)
+    local player = PlayerPedId()
+
+    if (not PlayerData.IsJailed and not isJailed) or ( PlayerData.IsJailed and isJailed) then
+        return
+    end
+
+    local NearbyJail = GetNearbyJail()
+
+    if not NearbyJail then
+        return
+    end
+
+    
+    while not IsScreenFadedOut() do
+        Wait(50)
+        DoScreenFadeOut(2000)
+    end
+
+    TaskStandStill(player, -1)
+
+    PlayerData.IsJailed = isJailed
+
+    if not isJailed then
+        TPZ.TeleportToCoords(NearbyJail.JailOutCoords.x, NearbyJail.JailOutCoords.y, NearbyJail.JailOutCoords.z, NearbyJail.JailOutCoords.h)
+    else
+        TPZ.TeleportToCoords(NearbyJail.JailInCoords.x, NearbyJail.JailInCoords.y, NearbyJail.JailInCoords.z, NearbyJail.JailInCoords.h)
+        OnJailedCharacterThread()
+    end
+
+    Wait(4000)
+    DoScreenFadeIn(2000)
+    Wait(1000)
+    TaskStandStill(player, 1)
+end)
+
+
+-----------------------------------------------------------
+--[[ Threads ]]--
+-----------------------------------------------------------
+
+function OnJailedCharacterThread()
+
+    Citizen.CreateThread(function()
+
+        while true do
+
+            Wait(60000)
+
+            if not PlayerData.IsJailed then
+                break
+            end
+
+            TriggerServerEvent("tpz_police:server:requestJailTime")
+        end
+    
+    end)
+
+end
